5827eb797772ea3184143e4c151ea2136b8b4d6e
Gesang Widigdo <gesangwidigdo@gmail.com>
2025-04-14 14:24:56 +0700

    refactor: move show dialog function

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/model/task.dart';
+import 'package:todo_app/util/show_task_dialog.dart';
 import 'package:todo_app/widget/todo_card.dart';
-import 'package:todo_app/widget/form_button.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
@@ -50,89 +50,26 @@ class MainScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _addDialog(context),
+        onPressed:
+            () => showTaskDialog(
+              context: context,
+              title: 'Add Task',
+              taskController: taskController,
+              dateController: dateController,
+              initialDate: DateTime.now(),
+              onSave: () {
+                if (taskController.text.isNotEmpty) {
+                  final task = Task(
+                    task: taskController.text,
+                    deadline: DateTime.parse(dateController.text),
+                  );
+                  _addTask(task);
+                }
+              },
+            ),
         backgroundColor: Colors.lime,
         child: Icon(Icons.add, color: Colors.indigo),
       ),
-    );
-  }
-
-  Future<void> _addDialog(BuildContext context) {
-    // Date and time variables
-    DateTime selectedDate = DateTime.now();
-
-    // Show date picker
-    void _selectDate() async {
-      final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2000),
-        lastDate: DateTime(2101),
-      );
-      if (picked != null && picked != selectedDate) {
-        selectedDate = picked;
-        dateController.text = "${selectedDate.toLocal()}".split(' ')[0];
-      }
-    }
-
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Add Task'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextFormField(
-                controller: taskController,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Task Name',
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: dateController,
-                decoration: const InputDecoration(
-                  labelText: 'Select Deadline Date',
-                  border: UnderlineInputBorder(),
-                ),
-                readOnly: true,
-                onTap: () {
-                  _selectDate();
-                },
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  FormButton(
-                    buttonText: 'Cancel',
-                    backgroundColor: Colors.red,
-                    buttonFunction: () => Navigator.of(context).pop(),
-                  ),
-                  const SizedBox(width: 8),
-                  FormButton(
-                    buttonText: 'Add',
-                    backgroundColor: Colors.green,
-                    buttonFunction: () {
-                      print(taskController.text);
-                      if (taskController.text.isNotEmpty) {
-                        final task = Task(
-                          task: taskController.text,
-                          deadline: selectedDate,
-                        );
-                        _addTask(task);
-                        Navigator.pop(context);
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/util/show_task_dialog.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/model/task.dart';
+import 'package:todo_app/widget/form_button.dart';
+
+Future<void> showTaskDialog({
+  required BuildContext context,
+  required String title,
+  required TextEditingController taskController,
+  required TextEditingController dateController,
+  required DateTime initialDate,
+  required VoidCallback onSave,
+}) async {
+  // Date and time variables
+  DateTime selectedDate = initialDate;
+
+  // Show date picker
+  void _selectDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      selectedDate = picked;
+      dateController.text = "${selectedDate.toLocal()}".split(' ')[0];
+    }
+  }
+
+  await showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              controller: taskController,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Task Name',
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: dateController,
+              decoration: const InputDecoration(
+                labelText: 'Select Deadline Date',
+                border: UnderlineInputBorder(),
+              ),
+              readOnly: true,
+              onTap: () {
+                _selectDate();
+              },
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FormButton(
+                  buttonText: 'Cancel',
+                  backgroundColor: Colors.red,
+                  buttonFunction: () => Navigator.of(context).pop(),
+                ),
+                const SizedBox(width: 8),
+                FormButton(
+                  buttonText: 'Save',
+                  backgroundColor: Colors.green,
+                  buttonFunction: () {
+                    onSave();
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
